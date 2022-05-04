275b4f44c624f81343050bf4e6bd88fb93f988de
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-04 16:14:01 +0200

    [*] Update base compilation unit

---
+++ client/src/entrypoint.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** mes_equipes
+** File description:
+** entrypoint
+*/
+
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        printf("Usage: ./mes_equipes <team_name>\n");
+        return (84);
+    }
+    return (0);
+}
---
+++ server/src/entrypoint.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2022
+** mes_equipes
+** File description:
+** entrypoint
+*/
+
+
+int main(int ac, char **av)
+{
+    return (0);
+}
