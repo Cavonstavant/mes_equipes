9d0b5fa182edae1962ae06169c9a86c64506beb8
osvegn <thomas.prudhomme@epitech.eu>
2022-05-09 10:13:02 +0200

    [+] create global variable with command from client

--- server/src/parser/check_command.c
+++ server/src/parser/check_command.c
@@ -5,10 +5,28 @@
 ** check_command
 */
 
+#include "parser.h"
+#include <stddef.h>
+
+static const cli_command_t *commands[14] = {
+    {"/help", NULL, &help_command},
+    {"/login", {""}, &login_command},
+    {"/logout", NULL, &logout_command},
+    {"/users", NULL, &users_command},
+    {"/user", {""}, &user_command},
+    {"/send", {"", ""}, &send_command},
+    {"/messages", {""}, &messages_command},
+    {"/subscribe", {""}, &subscribe_command},
+    {"/subscribed", {""}, &subscribed_command},
+    {"/unsubscribe", {""}, &unsubscribe_command},
+    {"/use", {""}, &use_command},
+    {"/create", {"", ""}, &create_command},
+    {"/list", NULL, &list_command},
+    {"/info", NULL, &info_command}
+};
+
 int check_parser(char **arguments)
 {
-    for (int index = 0; arguments[index]; index++) {
-        printf("%s\n", arguments[index]);
-    }
+
     return (0);
 }
