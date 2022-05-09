17c0f7534357227ab7b7575565c15fdd4d8f6e08
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-09 10:40:31 +0200

    [+] Add Cmake list for the objects library Close #19

---
+++ CMakeFiles/fclean.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fclean"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/fclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/src/CMakeFiles/myteams_cli.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../myteams_cli"
+  "../../myteams_cli.pdb"
+  "CMakeFiles/myteams_cli.dir/entrypoint.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myteams_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
