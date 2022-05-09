e3ead281240641ebeede08376e00883099fd67fb
osvegn <thomas.prudhomme@epitech.eu>
2022-05-09 10:29:14 +0200

    [+] add count_tab_len function for parser #30

--- server/src/parser/check_command.c
+++ server/src/parser/check_command.c
@@ -10,23 +10,34 @@
 
 static const cli_command_t *commands[14] = {
     {"/help", NULL, &help_command},
-    {"/login", {""}, &login_command},
+    {"/login", {"", NULL}, &login_command},
     {"/logout", NULL, &logout_command},
     {"/users", NULL, &users_command},
-    {"/user", {""}, &user_command},
-    {"/send", {"", ""}, &send_command},
-    {"/messages", {""}, &messages_command},
-    {"/subscribe", {""}, &subscribe_command},
-    {"/subscribed", {""}, &subscribed_command},
-    {"/unsubscribe", {""}, &unsubscribe_command},
-    {"/use", {""}, &use_command},
-    {"/create", {"", ""}, &create_command},
+    {"/user", {"", NULL}, &user_command},
+    {"/send", {"", "", NULL}, &send_command},
+    {"/messages", {"", NULL}, &messages_command},
+    {"/subscribe", {"", NULL}, &subscribe_command},
+    {"/subscribed", {"", NULL}, &subscribed_command},
+    {"/unsubscribe", {"", NULL}, &unsubscribe_command},
+    {"/use", {"", NULL}, &use_command},
+    {"/create", {"", "", NULL}, &create_command},
     {"/list", NULL, &list_command},
     {"/info", NULL, &info_command}
 };
 
+static int count_tab_len(char **tab)
+{
+    int arg_count = 0;
+
+    while (tab[arg_count]) {
+        arg_count++;
+    }
+    return (arg_count);
+}
+
 int check_parser(char **arguments)
 {
+    int arg_count = count_tab_len(arguments);
 
     return (0);
 }
