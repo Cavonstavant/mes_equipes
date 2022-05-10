10385d4dcc98c50d96bf2ae76820f760cacc87f3
osvegn <thomas.prudhomme@epitech.eu>
2022-05-10 17:25:10 +0200

    [+] add documentation for check_arguments function

--- server/src/parser/include/parser.h
+++ server/src/parser/include/parser.h
@@ -10,6 +10,10 @@
 #ifndef PARSER_H_
     #define PARSER_H_
 
+/// \brief This function check if arguments are valid, depending of the first
+/// element on the array.
+/// \param char ** Array of arguments which will be check.
+/// \return 0 if arguments are not good, 1 otherwise.
 int check_arguments(char **arguments);
 
 #endif /* !PARSER_H_ */
