0cda075a2643649fe00b55548318e780aca6f217
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-11 15:41:13 +0200

    [+] Add missing documentation on my_uuid_wrapper

--- objects/uuid/uuid_wrapper/src/include/my_uuid.h
+++ objects/uuid/uuid_wrapper/src/include/my_uuid.h
@@ -25,15 +25,6 @@ typedef struct my_uuid_s {
 /// \note Each prefix is 4 characters long
 extern const char *TEAMS_UUID_PREFIXES[];
 
-/// \brief UUID prefix getter
-/// \param prefix_id The prefix index, please use the proper macros in order
-/// to access the correct prefix
-/// \return The uuid prefix
-static inline const char *get_prefix(int prefix_idx)
-{
-    return TEAMS_UUID_PREFIXES[prefix_idx];
-}
-
 typedef enum my_uuid_prefix_e {
     /// \brief Team UUID
     MY_UUID_PREFIX_TEAM,
@@ -53,6 +44,15 @@ typedef enum my_uuid_prefix_e {
     MY_UUID_PREFIX_COUNT
 } my_uuid_prefix;
 
+/// \brief UUID prefix getter
+/// \param prefix_id The prefix index, please use the proper macros in order
+/// to access the correct prefix
+/// \return The uuid prefix
+static inline const char *get_prefix(my_uuid_prefix id)
+{
+    return TEAMS_UUID_PREFIXES[id];
+}
+
 /// \brief Generates a new uuid
 /// \param out The output uuid
 /// \param prefix The prefix to be inserted before the uuid
@@ -65,16 +65,25 @@ int my_uuid_generate(my_uuid_t *out, my_uuid_prefix prefix);
 /// \return 0 on success, -1 on failure
 int my_uuid_copy(my_uuid_t *dst, my_uuid_t *src);
 
+/// \brief Displays a given uuid on a specific stream
 static inline void display_uuid(my_uuid_t *uuid, FILE *stream)
 {
     fprintf(stream, "%s\n", uuid->repr);
 }
 
+/// \brief compare whether two UUIDs are the same
+/// \note wrapper around uuid_compare
+/// \return Returns an integer less than, equal to, or greater than zero
+/// if uu1 is found,
+/// respectively, to be lexicographically less than, equal, or greater than uu2
 static inline int my_uuid_compare(my_uuid_t uu1, my_uuid_t uu2)
 {
+    if (strncmp(uu1.repr, uu2.repr, 4) != 0)
+        return (strncmp(uu1.repr, uu2.repr, 4));
     return (uuid_compare(uu1.uuid, uu2.uuid));
 }
 
+/// \brief get the my_uuid_prefix representation of a given uuid
 my_uuid_prefix my_uuid_get_prefix(my_uuid_t *uuid);
 
 #endif /* MY_UUID_H */
