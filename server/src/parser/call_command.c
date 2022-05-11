28dd534a3ea20fd4af648a086871d423f9e16e80
osvegn <thomas.prudhomme@epitech.eu>
2022-05-11 16:46:52 +0200

    [+] implement function call command that call the correct command

--- server/src/parser/call_command.c
+++ server/src/parser/call_command.c
@@ -5,7 +5,17 @@
 ** call_command
 */
 
+#include "cli_commands.h"
+#include <stddef.h>
+
 int call_command(char **arguments)
 {
+    cli_command_t *commands = get_cli_commands();
+    void *retval = NULL;
+
+    for (int i = 0; i < 14; i++) {
+        if (strcmp(arguments[0], commands[i].name) == 0)
+            retval = commands[i].function(NULL);
+    }
     return (0);
 }
