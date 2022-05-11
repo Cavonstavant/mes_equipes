f7ff76c0815e69e24ecd3d983af2b48f3304f1a6
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-11 11:22:07 +0200

    [+] Adding Objects Constructor : Body Init #20

--- objects/body/include/object_body.h
+++ objects/body/include/object_body.h
@@ -10,10 +10,25 @@
 #ifndef OBJECT_BODY_H_
     #define OBJECT_BODY_H_
 
+    #include <stddef.h>
+
+    #define BODY_SIZE 512
+    #define BODY_ERROR NULL
+    #define BODY_INVALID_SIZE "Invalid content size."
+
 ///
 /// \brief Body object equivalent of a string
 ///
 ///
-typedef char body_t[512];
+typedef char *body_t;
+
+///
+/// \brief Init the Body object
+/// If the body_content is too long compared to BODY_SIZE, return BODY_ERROR
+///
+/// \param body_content Content of the new body object
+/// \return body_t The newly created body object
+///
+body_t body_init(char *body_content);
 
 #endif /* !OBJECT_BODY_H_ */
--- objects/body/src/object_body.c
+++ objects/body/src/object_body.c
@@ -8,3 +8,22 @@
 /// \file objects/body/src/object_body.c
 
 #include "object_body.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+body_t body_init(char *body_content)
+{
+    body_t new_body;
+
+    if (strlen(body_content) > BODY_SIZE) {
+        printf("%s Body init : %s.",BODY_INVALID_SIZE, body_content);
+        return BODY_ERROR;
+    }
+    new_body = malloc(sizeof(body_t) * (BODY_SIZE + 1));
+    if (new_body == BODY_ERROR)
+        return BODY_ERROR;
+    new_body[0] = '\0';
+    strcat(new_body, body_content);
+    return new_body;
+}
