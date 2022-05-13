3be265a2b620366c5936fcc62346a6f4e2ce8146
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-13 14:55:37 +0200

    [+] Adding testing Objects : Thread #20

---
+++ tests/unit_tests/objects/thread/objects_thread_tests.c
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** objects_my_thread_tests
+*/
+
+#include <criterion/criterion.h>
+#include "object_thread.h"
+
+Test(objects_my_thread, classic) {
+    thread_t *my_thread = thread_init((thread_creation_t) {
+        "My_Comment",
+        "My description",
+        my_uuid_init(USR_),
+        my_uuid_init(CHA_)
+    });
+
+    if (my_thread == NULL)
+        cr_assert_eq(1, 2);
+    cr_assert_eq(2, 2);
+}
+
+Test(objects_my_thread, invalid_name) {
+    thread_t *my_thread = thread_init((thread_creation_t) {
+        "My_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_CommentMy_Comment",
+        "My_description",
+        my_uuid_init(USR_),
+        my_uuid_init(CHA_)
+    });
+
+    if (my_thread != NULL)
+        cr_assert_eq(1, 2);
+    cr_assert_eq(2, 2);
+}
+
+Test(objects_my_thread, invalid_desc) {
+    thread_t *my_thread = thread_init((thread_creation_t) {
+        "My_Comment",
+        "My descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy descriptionMy description",
+        my_uuid_init(USR_),
+        my_uuid_init(CHA_)
+    });
+
+    if (my_thread != NULL)
+        cr_assert_eq(1, 2);
+    cr_assert_eq(2, 2);
+}
+
+Test(objects_my_thread, destroy_classic) {
+    thread_t *my_thread = thread_init((thread_creation_t) {
+        "My_Comment",
+        "My description",
+        my_uuid_init(USR_),
+        my_uuid_init(CHA_)
+    });
+
+    thread_destroy(my_thread);
+}
