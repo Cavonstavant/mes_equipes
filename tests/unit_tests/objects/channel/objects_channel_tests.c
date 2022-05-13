c11b217ba73b610c0012cbb5c9ffd67fad33464d
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-13 13:55:58 +0200

    [+] Adding testing Objects : Channel #20

---
+++ tests/unit_tests/objects/channel/objects_channel_tests.c
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** objects_my_channel_tests
+*/
+
+#include <criterion/criterion.h>
+#include "object_channel.h"
+
+Test(objects_my_channel, classic) {
+    channel_t *my_channel = channel_init((channel_creation_t) {
+        "My_Channel",
+        "I'm a glorious channel",
+        my_uuid_init(TEM_)
+    });
+
+    if (my_channel == NULL)
+        cr_assert_eq(1, 2);
+    cr_assert_eq(2, 2);
+}
+
+Test(objects_my_channel, invalid_name) {
+    channel_t *my_channel = channel_init((channel_creation_t) {
+        "My_Channelllllllllllllllllllllllllllllllllllllll",
+        "I'm a glorious channel",
+        my_uuid_init(TEM_)
+    });
+
+    if (my_channel != NULL)
+        cr_assert_eq(1, 2);
+    cr_assert_eq(2, 2);
+}
+
+Test(objects_my_channel, invalid_desc) {
+    channel_t *my_channel = channel_init((channel_creation_t) {
+        "You lose the game",
+        "I'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channelI'm a glorious channel",
+        my_uuid_init(TEM_)
+    });
+
+    if (my_channel != NULL)
+        cr_assert_eq(1, 2);
+    cr_assert_eq(2, 2);
+}
+
+Test(objects_my_channel, destroy_classic) {
+    channel_t *my_channel = channel_init((channel_creation_t) {
+        "My_Channel",
+        "I'm a glorious channel",
+        my_uuid_init(TEM_)
+    });
+
+    channel_destroy(my_channel);
+}
