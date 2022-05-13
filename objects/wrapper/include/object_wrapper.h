ab083b0dd5c28804ffa7201929089f646f4eb70e
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-13 17:51:44 +0200

    [+] Add Wrapper Object : Init #44

--- objects/wrapper/include/object_wrapper.h
+++ objects/wrapper/include/object_wrapper.h
@@ -18,27 +18,57 @@
     #include "object_team.h"
     #include "object_thread.h"
     #include "object_user.h"
+    #include <stddef.h>
+
+    ///
+    /// \brief WError inside a Wrapper object
+    ///
+    #define WRAPPER_ERROR NULL
 
 ///
 /// \brief Wrapper object, containing all Objects instance
 ///
 typedef struct object_wrapper_s {
-    /// List of all the body
-    channel_t **bodys;
+    /// List of all the channel
+    channel_t **channels;
+    /// Number of channels
+    int channel_n;
     /// List of all the comment
     comment_t **comments;
+    /// Number of comments
+    int comment_n;
     /// List of all the conversation
     conversation_t **conversations;
+    /// Number of conversations
+    int conversation_n;
     /// List of all the description
     description_t **descriptions;
+    /// Number of descriptions
+    int description_n;
     /// List of all the message
     message_t **messages;
+    /// Number of messages
+    int message_n;
     /// List of all the team
     team_t **teams;
+    /// Number of teams
+    int team_n;
     /// List of all the thread
     thread_t **threads;
+    /// Number of threads
+    int thread_n;
     /// List of all the user
     user_t **users;
+    /// Number of users
+    int user_n;
 } object_wrapper_t;
+
+///
+/// \brief Create a new wrapper object
+/// Init all the object lists
+///
+/// \return object_wrapper_t* Newly created wrapper
+///
+object_wrapper_t *wrapper_init(void);
 
 #endif /* !OBJECT_WRAPPER_H_ */
--- objects/wrapper/src/object_wrapper.c
+++ objects/wrapper/src/object_wrapper.c
@@ -8,3 +8,56 @@
 /// \file objects/wrapper/src/object_wrapper.c
 
 #include "object_wrapper.h"
+#include <stdlib.h>
+
+///
+/// \brief Init all the object lists
+///
+/// \param wrapper Current wrapper object
+/// \return object_wrapper_t* Newly filled wrapper object
+///
+static object_wrapper_t *wrapper_init_content(object_wrapper_t *wrapper)
+{
+    if ((wrapper->conversations = malloc(sizeof(conversation_t *)))
+        == CONVERSATION_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->conversation_n = 0;
+    if ((wrapper->descriptions = malloc(sizeof(description_t *)))
+        == DESCRIPTION_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->description_n = 0;
+    if ((wrapper->conversations = malloc(sizeof(conversation_t *)))
+        == CONVERSATION_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->conversation_n = 0;
+    if ((wrapper->messages = malloc(sizeof(message_t *))) == MESSAGE_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->message_n = 0;
+    if ((wrapper->teams = malloc(sizeof(team_t *))) == TEAM_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->team_n = 0;
+    return wrapper;
+}
+
+object_wrapper_t *wrapper_init(void)
+{
+    object_wrapper_t *wrapper = malloc(sizeof(object_wrapper_t) * 1);
+
+    if (wrapper == NULL)
+        return WRAPPER_ERROR;
+    if ((wrapper->channels = malloc(sizeof(channel_t *))) == CHANNEL_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->channel_n = 0;
+    if ((wrapper->comments = malloc(sizeof(comment_t *))) == COMMENT_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->comment_n = 0;
+    if ((wrapper->users = malloc(sizeof(user_t *))) == USER_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->user_n = 0;
+    if ((wrapper->threads = malloc(sizeof(thread_t *))) == THREAD_ERROR)
+        return WRAPPER_ERROR;
+    wrapper->thread_n = 0;
+    if (wrapper_init_content(wrapper) == WRAPPER_ERROR)
+        return WRAPPER_ERROR;
+    return wrapper;
+}
