49e5266c836316128a522cafa332574e1799bd8e
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-16 15:06:29 +0200

    [+] Add Wrapper dump : Testing #27

--- objects/wrapper/src/component_dump.c
+++ objects/wrapper/src/component_dump.c
@@ -16,29 +16,29 @@
 ///
 static void wrapper_dump_user(object_wrapper_t *wrapper)
 {
-    printf("\t%i Users :\n", wrapper->user_n);
+    printf("--- %i Users ---\n", wrapper->user_n);
     for (int i = 0; i < wrapper->user_n; i++)
         user_dump(wrapper->users[i]);
 }
 
 void wrapper_dump(object_wrapper_t *wrapper)
 {
-    printf("\t%i Channels :\n", wrapper->channel_n);
+    printf("--- %i Channels ---\n", wrapper->channel_n);
     for (int i = 0; i < wrapper->channel_n; i++)
         channel_dump(wrapper->channels[i]);
-    printf("\t%i Comments :\n", wrapper->comment_n);
+    printf("--- %i Comments ---\n", wrapper->comment_n);
     for (int i = 0; i < wrapper->comment_n; i++)
         comment_dump(wrapper->comments[i]);
-    printf("\t%i Conversations :\n", wrapper->conversation_n);
+    printf("--- %i Conversations ---\n", wrapper->conversation_n);
     for (int i = 0; i < wrapper->conversation_n; i++)
         conversation_dump(wrapper->conversations[i]);
-    printf("\t%i Messages :\n", wrapper->message_n);
+    printf("--- %i Messages ---\n", wrapper->message_n);
     for (int i = 0; i < wrapper->message_n; i++)
         message_dump(wrapper->messages[i]);
-    printf("\t%i Teams :\n", wrapper->team_n);
+    printf("--- %i Teams ---\n", wrapper->team_n);
     for (int i = 0; i < wrapper->team_n; i++)
         team_dump(wrapper->teams[i]);
-    printf("\t%i Threads :\n", wrapper->thread_n);
+    printf("--- %i Threads ---\n", wrapper->thread_n);
     for (int i = 0; i < wrapper->thread_n; i++)
         thread_dump(wrapper->threads[i]);
     wrapper_dump_user(wrapper);
