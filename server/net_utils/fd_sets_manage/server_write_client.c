ffe60b3df14c2930ef8ba5c1f1f7c622ee5963ed
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-17 23:38:37 +0200

    [*] Update server_write_client return type

--- server/net_utils/fd_sets_manage/server_write_client.c
+++ server/net_utils/fd_sets_manage/server_write_client.c
@@ -7,7 +7,7 @@
 
 #include "net_utils.h"
 
-void server_write_client(tcp_server_t *srv, peer_t *tmp) {
+bool server_write_client(tcp_server_t *srv, peer_t *tmp) {
     (void) srv;
     (void) tmp;
 }
