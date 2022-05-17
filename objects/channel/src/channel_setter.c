4d37bbe1e4192d2a90327ba37d71467f51cdb1ad
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-17 11:30:31 +0200

    [+] Add Object Setter : Channel #39

--- objects/channel/src/channel_setter.c
+++ objects/channel/src/channel_setter.c
@@ -8,11 +8,31 @@
 /// \file objects/channel/src/channel_dump.c
 
 #include "object_channel.h"
+#include <stdlib.h>
 
 void channel_edit_uuid(channel_t *channel, char *uuid)
 {
-    (void) channel;
-    (void) uuid;
-    /// TO REFACTO WHEN UUID CHANGE IMPLEMENTED
+    my_uuid_edit_content(channel->uuid, uuid);
+}
+
+void channel_edit_name(channel_t *channel, char *name)
+{
+    name_edit_content(channel->name, name);
+}
+
+void channel_edit_parent(channel_t *channel, char *parent)
+{
+    my_uuid_edit_content(channel->team, parent);
+}
+
+bool channel_add_threads(channel_t *channel, my_uuid_t *thread)
+{
+    channel->threads = realloc(channel->threads,
+    sizeof(my_uuid_t *) * (channel->threads_n + 2));
+    if (channel->threads == NULL)
+        return OPERATION_FAILED;
+    channel->threads[channel->threads_n] = thread;
+    channel->threads_n += 1;
+    return OPERATION_SUCCESS;
 }
 
