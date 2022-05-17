94f6fabc5ec25de8aeb564a3245bb6e139ac6496
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-17 19:15:21 +0200

    [+] Add Object Setter : Message #43

---
+++ objects/message/src/message_setter.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** message_setter
+*/
+
+/// \file objects/message/src/message_setter.c
+
+#include "object_message.h"
+
+void message_edit_uuid(message_t *message, char *uuid)
+{
+    my_uuid_edit_content(message->uuid, uuid);
+}
+
+void message_edit_body(message_t *message, char *body)
+{
+    body_edit_content(message->body, body);
+}
+
+void message_edit_time(message_t *message, char *time)
+{
+    (void) message;
+    (void) time;
+    /// IMPLEMENTING LATER
+}
