e93a396a79e3c014d74d75fc753e0de6f71c7ddb
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-17 23:38:14 +0200

    [+] Add server_close_client function

--- server/net_utils/fd_sets_manage/server_close_client.c
+++ server/net_utils/fd_sets_manage/server_close_client.c
@@ -8,6 +8,8 @@
 #include "net_utils.h"
 
 bool server_close_client(tcp_server_t *srv, peer_t *tmp) {
-    (void) srv;
-    (void) tmp;
+    if (!tmp || !srv)
+        return (false);
+    srv->sockets_to_be_removed[tmp->sock_fd] = true;
+    return (true);
 }
