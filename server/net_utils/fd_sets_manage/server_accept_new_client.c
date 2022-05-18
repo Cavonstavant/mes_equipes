8d48d415d7bf33691bc1eee9576dda5e69f66af8
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-18 16:25:02 +0200

    [*] Refactor new client connection function

--- server/net_utils/fd_sets_manage/server_accept_new_client.c
+++ server/net_utils/fd_sets_manage/server_accept_new_client.c
@@ -18,16 +18,15 @@ static peer_t *accept_client(tcp_server_t *srv)
         (struct sockaddr *)&client_addr,
         &client_addr_size);
     if (client_fd < 0)
-        HANDLE_ERROR("accept");
+        TEAMS_LOG("accept");
     new_client = new_peer(client_fd, client_addr);
     if (new_client == NULL)
-        HANDLE_ERROR("peer_create");
+        TEAMS_LOG("peer_create");
     FD_SET(client_fd, &srv->read_fds);
-    new_client->data = fill_client_data(srv->arbitrary_data);
     return (new_client);
 }
 
-static bool manage_new_conn(tcp_server_t *srv, fd_set *read_fs, peer_t *new)
+static bool manage_new_conn(tcp_server_t *srv, peer_t *new)
 {
     new = accept_client(srv);
     if (new == NULL){
@@ -42,5 +41,5 @@ bool server_accept_new_client(tcp_server_t *srv)
 {
     peer_t *new = NULL;
 
-    return (manage_new_conn(srv, &srv->read_fds, new));
+    return (manage_new_conn(srv, new));
 }
