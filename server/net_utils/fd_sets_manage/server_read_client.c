3246fcf1965ee7ff4161810065cbdae8f57a1127
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-18 15:21:21 +0200

    [+] Implement server_read_client function

--- server/net_utils/fd_sets_manage/server_read_client.c
+++ server/net_utils/fd_sets_manage/server_read_client.c
@@ -9,6 +9,18 @@
 
 bool server_read_client(tcp_server_t *srv, peer_t *tmp)
 {
-    (void) srv;
-    (void) tmp;
+    if (!tmp && !srv)
+        return (false);
+    ssize_t read_size;
+
+    read_size = read(tmp->sock_fd, tmp->input_buffer, MAX_MSG);
+    if (read_size < 0){
+        TEAMS_LOG("read");
+        return (false);
+    }
+    if (read_size == 0){
+        TEAMS_LOG("Client disconnected");
+        return (false);
+    }
+    return (true);
 }
