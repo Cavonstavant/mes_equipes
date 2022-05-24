ae290a1fc06f14d99168d6e037d7fb2bb02d8e1c
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-24 13:36:58 +0200

    [+] Add Object Getter : Comment Testing #28

--- tests/unit_tests/objects/wrapper/lower_component_get_tests.c
+++ tests/unit_tests/objects/wrapper/lower_component_get_tests.c
@@ -19,3 +19,16 @@ Test(objects_my_wrapper, cmp_message) {
     cr_assert_null(wrapper_find_message(my_wrapper, my_uuid_init(TEM_)));
     cr_assert_not_null(wrapper_find_message(my_wrapper, my_wrapper->messages[0]->uuid));
 }
+
+Test(objects_my_wrapper, cmp_comment) {
+    object_wrapper_t *my_wrapper = wrapper_init();
+
+    wrapper_adding_comment(my_wrapper, (comment_creation_t) {
+       "Title",
+       my_uuid_init(TEM_),
+       my_uuid_init(USR_)
+
+    });
+    cr_assert_null(wrapper_find_comment(my_wrapper, my_uuid_init(TEM_)));
+    cr_assert_not_null(wrapper_find_comment(my_wrapper, my_wrapper->comments[0]->uuid));
+}
