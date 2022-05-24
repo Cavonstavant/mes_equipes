77ed636651b88a88038d479c6a8d49d3a9b96164
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-24 13:13:19 +0200

    [+] Add Object Getter : User #28

--- objects/wrapper/src/upper_component_get.c
+++ objects/wrapper/src/upper_component_get.c
@@ -9,6 +9,15 @@
 
 #include "object_wrapper.h"
 
+user_t *wrapper_find_user(object_wrapper_t *wrapper, my_uuid_t *find)
+{
+    for (int i = 0; i < wrapper->user_n; i++) {
+        if (my_uuid_cmp(wrapper->users[i]->uuid, find))
+            return wrapper->users[i];
+    }
+    return OBJECT_NOT_FIND;
+}
+
 channel_t *wrapper_find_channel(object_wrapper_t *wrapper, my_uuid_t *find)
 {
     for (int i = 0; i < wrapper->channel_n; i++) {
