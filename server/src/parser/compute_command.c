66c77d89d77ae8bdf1de5fdcf5481db311c326bd
osvegn <thomas.prudhomme@epitech.eu>
2022-05-24 23:51:27 +0200

    [+] add a function in compute command that allow more flexible command from the client #67

--- server/src/parser/compute_command.c
+++ server/src/parser/compute_command.c
@@ -10,10 +10,57 @@
 #include <stdlib.h>
 #include "cli_commands.h"
 
+/// \brief Format the flexible argument part of the command to strict
+/// argument format.
+/// \param char * The flexible command.
+/// \param int The index in the flexible command.
+/// \param int The index in the strict command.
+/// \return char * The strict command.
+static char *format_arguments(char *command, int cmd_i, int new_cmd_i)
+{
+    int copy = 0;
+
+    for (; command[cmd_i]; cmd_i++) {
+        if (command[cmd_i] == '"' && copy)
+            command[new_cmd_i] = '"';
+        else if (command[cmd_i] == '"' && !copy)
+            command[new_cmd_i] = ' ';
+        if (command[cmd_i] == '"') {
+            copy = (copy) ? 0 : 1;
+            new_cmd_i++;
+        }
+        if (copy) {
+            command[new_cmd_i] = command[cmd_i];
+            new_cmd_i++;
+        }
+    }
+    command[new_cmd_i] = '\0';
+    return command;
+}
+
+/// \brief Format the command from flexible command format to strict
+/// command format.
+/// \param char * Flexible command.
+/// \return char * Strict command.
+static char *format_command(char *command)
+{
+    int cmd_i = 0;
+    int new_cmd_i = 0;
+
+    if (!command)
+        return NULL;
+    for (; command[cmd_i] && command[cmd_i] != '/'; cmd_i++);
+    for (; command[cmd_i] && command[cmd_i] != ' '; cmd_i++) {
+        command[new_cmd_i] = command[cmd_i];
+        new_cmd_i++;
+    }
+    return format_arguments(command, cmd_i, new_cmd_i);
+}
+
 int compute_command(char *command)
 {
-    char *tmp = command;
-    cli_command_t *cmd = convert_command_to_structure(command);
+    char *tmp = format_command(command);
+    cli_command_t *cmd = convert_command_to_structure(tmp);
 
     if (cmd == NULL) {
         return (-1);
@@ -21,6 +68,6 @@ int compute_command(char *command)
     if (call_command(cmd) < 0) {
         return (-1);
     }
-    free(tmp);
+    free(command);
     return (0);
 }
--- tests/unit_tests/parser/compute_command_test.c
+++ tests/unit_tests/parser/compute_command_test.c
@@ -9,98 +9,133 @@
 #include "parser.h"
 #include "cli_commands.h"
 
-Test(compute_command, test02)
+Test(compute_command, test01)
 {
     char *cmd = "/login \"thomas\"";
 
     cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
+Test(compute_command, test02)
+{
+    char *cmd = "/login      \"thomas\"";
+
+    cr_assert_eq(compute_command(strdup(cmd)), 0);
+}
+
 Test(compute_command, test03)
 {
-    char *cmd = "/help";
+    char *cmd = "     /login      \"thomas\"";
 
     cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
 Test(compute_command, test04)
 {
-    char *cmd = "/coucou";
+    char *cmd = "     /send   ZKPGJSOMIGJ   \"thomas\" azetz\"lucas\"";
 
-    cr_assert_eq(compute_command(strdup(cmd)), -1);
+    cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
 Test(compute_command, test05)
 {
-    char *cmd = "/coucou salut";
+    char *cmd = "/login \"thomas\"";
 
-    cr_assert_eq(compute_command(strdup(cmd)), -1);
+    cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
 Test(compute_command, test06)
 {
-    char *cmd = "/login salut";
+    char *cmd = "/help";
 
-    cr_assert_eq(compute_command(strdup(cmd)), -1);
+    cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
 Test(compute_command, test07)
 {
-    char *cmd = "/login ";
+    char *cmd = "/send \"thomas\"\"lucas\"";
 
     cr_assert_eq(compute_command(strdup(cmd)), -1);
 }
 
 Test(compute_command, test08)
 {
-    char *cmd = "/login   \"coucou\"";
+    char *cmd = "/coucou";
 
     cr_assert_eq(compute_command(strdup(cmd)), -1);
 }
 
 Test(compute_command, test09)
 {
-    char *cmd = "login \"thomas\"";
+    char *cmd = "/coucou salut";
 
     cr_assert_eq(compute_command(strdup(cmd)), -1);
 }
 
 Test(compute_command, test10)
 {
-    char *cmd = "/login \"thomas\" \"thomas\"";
+    char *cmd = "/login salut";
 
     cr_assert_eq(compute_command(strdup(cmd)), -1);
 }
 
 Test(compute_command, test11)
 {
-    char *cmd = "/login\"thomas\"";
+    char *cmd = "/login ";
 
     cr_assert_eq(compute_command(strdup(cmd)), -1);
 }
 
 Test(compute_command, test12)
 {
+    char *cmd = "/login   \"coucou\"";
+
+    cr_assert_eq(compute_command(strdup(cmd)), 0);
+}
+
+Test(compute_command, test13)
+{
+    char *cmd = "login \"thomas\"";
+
+    cr_assert_eq(compute_command(strdup(cmd)), -1);
+}
+
+Test(compute_command, test14)
+{
+    char *cmd = "/login \"thomas\" \"thomas\"";
+
+    cr_assert_eq(compute_command(strdup(cmd)), -1);
+}
+
+Test(compute_command, test15)
+{
+    char *cmd = "/login\"thomas\"";
+
+    cr_assert_eq(compute_command(strdup(cmd)), -1);
+}
+
+Test(compute_command, test16)
+{
     char *cmd = "/login \"thomas";
 
     cr_assert_eq(compute_command(strdup(cmd)), -1);
 }
 
-Test(compute_command, test13)
+Test(compute_command, test17)
 {
     char *cmd = "/help ";
 
     cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
-Test(compute_command, test14)
+Test(compute_command, test18)
 {
     char *cmd = "/send \"thomas\" \"salut\"";
 
     cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
 
-Test(convert_command_to_structure, test15)
+Test(convert_command_to_structure, test19)
 {
     char *cmd = strdup("/login \"thomas\"");
     cli_command_t *cli = convert_command_to_structure(cmd);
@@ -109,22 +144,29 @@ Test(convert_command_to_structure, test15)
     cr_assert_str_eq(cli->arguments[0], "thomas");
 }
 
-Test(compute_command, test16)
+Test(compute_command, test20)
 {
     char *cmd = NULL;
 
     cr_assert_eq(compute_command(cmd), -1);
 }
 
-Test(get_cli_commands, test17)
+Test(get_cli_commands, test21)
 {
     const cli_command_t *commands = get_cli_commands();
 
     cr_assert(commands != NULL);
 }
 
-Test(fill_command_arguments, test18)
+Test(fill_command_arguments, test22)
 {
     char **ret = fill_command_arguments(NULL, "/login");
     cr_assert(ret == NULL);
+}
+
+Test(compute_command, test23)
+{
+    char *cmd = "   /login \"SALTUZEOUGIESQ\"";
+
+    cr_assert_eq(compute_command(strdup(cmd)), 0);
 }
