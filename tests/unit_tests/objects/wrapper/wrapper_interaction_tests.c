1d02bb537698fbbdafc5c125021959bfdcac19c6
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-05-24 15:56:55 +0200

    [+] Add Wrapper Interaction : User leave team testing #44

--- tests/unit_tests/objects/wrapper/wrapper_interaction_tests.c
+++ tests/unit_tests/objects/wrapper/wrapper_interaction_tests.c
@@ -252,6 +252,11 @@ Test(objects_my_wrapper, user_journey_interaction) {
 
     cr_assert_eq(temp, true);
 
+    wrapper_user_join_team(my_wrapper, my_wrapper->users[0]->uuid, my_wrapper->teams[1]->uuid);
+    temp = wrapper_user_leave_team(my_wrapper, my_wrapper->users[0]->uuid, my_wrapper->teams[1]->uuid);
+
+    cr_assert_eq(temp, true);
+
     printf("\n\n\n\n\n-----USER JOURNEY------\n");
     fflush(NULL);
     wrapper_dump(my_wrapper);
