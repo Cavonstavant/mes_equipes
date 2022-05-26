b0514c8f6a79bfc9e49c074a319e77ba3a564888
osvegn <thomas.prudhomme@epitech.eu>
2022-05-26 18:06:22 +0200

    [+] udpate the function that call the correct return code for the client

--- server/src/commands/command_login.c
+++ server/src/commands/command_login.c
@@ -5,17 +5,18 @@
 ** login_command
 */
 
-#include "rcode.h"
+#include "rcodes.h"
 
 void *command_login(void *arg)
 {
     bool existing_user = false; // call command to know if client exist
     char *username = ""; // need to define username
+    char **cmd_args = NULL;
 
     if (existing_user) {
-        printf(rcode_201, username);
+        print_retcode(201, cmd_args);
     } else {
-        printf(rcode_202, username);
+        print_retcode(202, cmd_args);
     }
-    return ((void *)0);
+    return NULL;
 }
