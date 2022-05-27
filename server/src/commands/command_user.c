f0ba865112c1d0925e0cb71464ff6a083d405c52
osvegn <thomas.prudhomme@epitech.eu>
2022-05-27 22:28:36 +0200

    [+] create command_user function #5

--- server/src/commands/command_user.c
+++ server/src/commands/command_user.c
@@ -6,4 +6,24 @@
 */
 
 #include "rcodes.h"
+#include <stdbool.h>
 
+static bool find_uuid(char *uuid)
+{
+    (void)uuid;
+    if (1) //todo write the correct condition
+        return true;
+    return false;
+}
+
+void *command_user(void *args)
+{
+    char *uuid = NULL;
+    char **cmd_arg = NULL;
+
+    if (!find_uuid(uuid))
+        print_retcode(311, cmd_args);
+    else
+        print_retcode(200, cmd_args);
+    return NULL;
+}
