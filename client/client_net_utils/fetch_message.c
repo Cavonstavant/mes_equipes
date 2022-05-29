ce88680e92f522719ce038bbe3f3f5bd04d3487e
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-29 16:59:15 +0200

    [+] Add client fetch_message implementation

---
+++ client/client_net_utils/fetch_message.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2022
+** mes_equipes
+** File description:
+** fetch_message
+*/
+
+#include "client_utils.h"
+#include <string.h>
+#include <unistd.h>
+
+char *fetch_message(client_net_server_t *server)
+{
+    char *msg;
+    ssize_t read_size = 0;
+
+    if (!server)
+        return NULL;
+    server->pending_read = true;
+    read_size = read(server->sock_fd, server->pending_read, MAX_MSG);
+    if (read_size < 0) {
+        TEAMS_LOG("Internal Server Error: read\n");
+        return NULL;
+    }
+    if (read_size == 0) {
+        TEAMS_LOG("Connection to the server lost\n");
+        return NULL;
+    }
+    msg = strndup(server->read_buffer, strlen(server->read_buffer));
+    return msg;
+}
