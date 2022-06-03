6456789675d0726f98dfb69b245ba44d87173026
Cavonstavant <constant.vigneron@protonmail.com>
2022-06-03 11:28:25 +0200

    [+] Add support for 208 retcode

--- client/src/include/teams_responses.h
+++ client/src/include/teams_responses.h
@@ -230,7 +230,7 @@ static const server_response_t server_response_template[] = {
                 .creation_time = NULL
             }
         },
-        .callback = default_response_callback
+        .callback = client_208_response_callback
     },
     {
         .code = 209,
--- client/src/server_responses_callbacks/callback_208.c
+++ client/src/server_responses_callbacks/callback_208.c
@@ -1,3 +1,4 @@
+
 /*
 ** EPITECH PROJECT, 2022
 ** mes_equipes
@@ -5,3 +6,53 @@
 ** callback_208
 */
 
+#include "teams_responses.h"
+#include "logging_client.h"
+#include <stdio.h>
+
+static void update_response_data(server_response_t *resp, char *msg)
+{
+    if (!resp)
+        return;
+    if (!(resp->data.data.thread_response_data.thread_uuid = strtok(msg, ":")))
+        return;
+    if (!(resp->data.data.thread_response_data.user_uuid = strtok(msg, ":")))
+        return;
+    if (!(resp->data.data.thread_response_data.creation_time
+        = strtok(msg, ":")))
+        return;
+    if (!(resp->data.data.thread_response_data.thread_title
+        = strtok(NULL, ":")))
+        return;
+    if (!(resp->data.data.thread_response_data.thread_body
+        = strtok(NULL, ":")))
+        return;
+}
+
+static void call_api(server_response_t *res)
+{
+    if (!res)
+        return;
+    client_print_thread(res->data.data.thread_response_data.thread_uuid + 4,
+        res->data.data.thread_response_data.user_uuid + 4,
+        res->data.data.thread_response_data.creation_time,
+        res->data.data.thread_response_data.thread_title,
+        res->data.data.thread_response_data.thread_body);
+}
+
+void client_208_response_callback(void *data)
+{
+    server_response_t *resp = (server_response_t *)data;
+
+    if (!resp || !resp->message)
+        return;
+    resp->message = strchr(resp->message, ' ') + 1;
+    update_response_data(resp, resp->message);
+    call_api(resp);
+    update_response_data(resp, NULL);
+    while (resp->data.data.thread_response_data.thread_uuid
+        && resp->message) {
+        call_api(resp);
+        update_response_data(resp, NULL);
+    }
+}
