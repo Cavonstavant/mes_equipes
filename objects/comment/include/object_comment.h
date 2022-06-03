0c0fab866011ad3343066f5960f61f7758bc05e8
osvegn <thomas.prudhomme@epitech.eu>
2022-06-03 21:54:51 +0200

    [+] add missing documentation in objects/comment part

--- objects/comment/include/object_comment.h
+++ objects/comment/include/object_comment.h
@@ -14,15 +14,10 @@
     #include "object_body.h"
     #include <time.h>
 
-    ///
     /// \brief Error inside a comment object
-    ///
     #define COMMENT_ERROR NULL
 
-///
 /// \brief Comment object
-///
-///
 typedef struct comment_s {
     /// Uuid of the comment
     my_uuid_t *uuid;
@@ -36,10 +31,8 @@ typedef struct comment_s {
     time_t time;
 } comment_t;
 
-///
 /// \brief Comment creation object
 /// Object containing all the information for a comment creation
-///
 typedef struct comment_creation_s {
     /// Initial body for the comment creation
     char *body;
@@ -49,75 +42,48 @@ typedef struct comment_creation_s {
     my_uuid_t *author;
 } comment_creation_t;
 
-///
 /// \brief Init a comment object
-///
 /// \param content content of the comment object
 /// \return comment_t* Newly created comment object
-///
 comment_t *comment_init(comment_creation_t content);
 
-///
 /// \brief Destroy a comment object
 /// If the comment is COMMENT_ERROR, do nothing
-///
 /// \param comment Comment object to destroy
-///
 void comment_destroy(comment_t *comment);
 
-///
 /// \brief Dump a comment object
-///
 /// \param comment Comment object to display
-///
 void comment_dump(comment_t *comment);
 
-///
 /// \brief Edit the uuid of a comment object
-///
 /// \param comment Comment to edit
 /// \param uuid Uuid to change in the comment object
-///
 void comment_edit_uuid(comment_t *comment, char *uuid);
 
-///
 /// \brief Edit the parent of a comment object
-///
 /// \param comment Comment to edit
 /// \param parent Parent to change in the comment object
-///
 void comment_edit_parent(comment_t *comment, char *parent);
 
-///
 /// \brief Edit the author of a comment object
-///
 /// \param comment Comment to edit
 /// \param author Author to change in the comment object
-///
 void comment_edit_author(comment_t *comment, char *author);
 
-///
 /// \brief Edit the body of a comment object
-///
 /// \param comment Comment to edit
 /// \param body Body to change in the comment object
-///
 void comment_edit_body(comment_t *comment, char *body);
 
-///
 /// \brief Edit the time of a comment object
-///
 /// \param comment Comment to edit
 /// \param time Time to change in the comment object
-///
 void comment_edit_time(comment_t *comment, char *time);
 
-///
 /// \brief Return the time creation in string format
-///
 /// \param comment Comment object
 /// \return char* Time in string format
-///
 char *comment_get_time(comment_t *comment);
 
 #endif /* !OBJECT_COMMENT_H_ */
