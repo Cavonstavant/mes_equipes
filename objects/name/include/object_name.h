c88dc274bd54cb3dd06cf4285b55210946cb8698
osvegn <thomas.prudhomme@epitech.eu>
2022-06-03 22:03:15 +0200

    [+] add missing documentation in objects/name part

--- objects/name/include/object_name.h
+++ objects/name/include/object_name.h
@@ -12,57 +12,36 @@
 
     #include <stddef.h>
 
-    ///
     /// \brief Max size of a name object
-    ///
     #define NAME_SIZE 32
 
-    ///
     /// \brief Error inside a name object
-    ///
     #define NAME_ERROR NULL
 
-    ///
     /// \brief Name error message for bad size
-    ///
     #define NAME_INVALID_SIZE "Invalid content size"
 
-///
 /// \brief Name object equivalent of a string
-///
-///
 typedef char *name_t;
 
-///
 /// \brief Init a name object
-///
 /// \param name_content Content of the new name object
 /// \return name_t The newly created name object
 /// If the name_content is too long compared to NAME_SIZE, return NAME_ERROR
-///
 name_t name_init(char *name_content);
 
-///
 /// \brief Destroy a Name object
 /// If the name_to_destroy is NAME_ERROR, do nothing
-///
 /// \param name_to_destroy Name object to destroy
-///
 void name_destroy(name_t name_to_destroy);
 
-///
 /// \brief Dump a name object
-///
 /// \param name Name object to print
-///
 void name_dump(name_t name);
 
-///
 /// \brief Edit the content of a name object
-///
 /// \param name Name object to edit
 /// \param content Content to edit into the Name object
-///
 void name_edit_content(name_t name, char *content);
 
 #endif /* !OBJECT_NAME_H_ */
